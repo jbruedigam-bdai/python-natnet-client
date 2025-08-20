fade9181f6f835e6778a34311f4911ce200887cb
Jan Bruedigam <jbruedigam@theaiinstitute.com>
2025-08-20 20:00:15 +0200

    Try pointer shift

--- natnet_client/data_frame.py
+++ natnet_client/data_frame.py
@@ -14,8 +14,9 @@ class FramePrefix(PacketComponent):
 
     @classmethod
     def read_from_buffer(cls, buffer: PacketBuffer, protocol_version: Version) -> "FramePrefix":
+        buffer.pointer = 0
         val = buffer.read_int32()
-        print(val)
+        print(f"frame {val}")
         return FramePrefix(val)
 
 
